4a14d0b73d1eaf4257b231d0d2a09f5746b3acae
jamescrosbie <jamescrosbie@hotmail.co.uk>
2017-08-08 21:14:12 +0100

    travel time correction, html updated

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Isochrones
-## July 2017
+## Aug 2017
 
-Develop Isochrones around given list of point, journey times, threshold and journey type.  Based on the work of [Drew Fustin](http://blog.yhat.com/posts/isochrones-isocronut.html) (Mar 2017).
+Python 3.6
 
-Output written to bounds.js and used by isochrones.html to render the leaflet map. 
+Develop Isochrones around given list of points, journey times, threshold and journey type.  Based on the work of [Drew Fustin](http://blog.yhat.com/posts/isochrones-isocronut.html) (Mar 2017).
+
+Scales number of angles depending on journey time for more granularity.
+
+Output written to bounds.js and used by isochrones.html to render the leaflet map.  HTML then offers chance to export isochrones to GeoJSON. 
--- getGeo.py
+++ getGeo.py
@@ -1,27 +1,37 @@
 #Imports
 import configparser
-import simplejson
+import json
 from urllib.parse import urlparse
 from urllib.request import Request as urlrequest
 from urllib.request import build_opener
-from math import pi, cos, sin, radians, degrees, asin, atan2
+from math import pi, cos, sin, radians, degrees, asin, atan2, ceil
+import os
 
 
 #set constants
-home_string = ['HD1+2PQ', 'LS2+7UA']
-config_path ='/home/'
-output_path = ''
-no_of_angles = 12  # best multipal of 12
-travel_methods = ['driving','bicycling','transit','walking']
-travel_method = travel_methods[0]
-travel_time_limit = 20 # mins
-tolerance = 5 #mins
+config_path = '/home/james/Documents/Projects/DataViz/'
+output_path = '/home/james/Documents/Projects/DataViz/isochrones/data/'
+travel_methods = ['driving', 'bicycling', 'transit', 'walking']
+
+#my parameters
+home_string = ['HD1+1BA']
+travel_method = travel_methods[2]
+travel_time_limit = 40 # mins
+tolerance = 1 #mins
 
 
-def build_url(address_str='', config_path='/home/' ):
+def initialise():
+#remove response logger
+    try:
+        os.remove(output_path+'statusFile.csv')
+    except:
+        print("No response logger")
+
+ 
+def build_url(address_str='', config_path='/home/'):
     #read in api_key
     config = configparser.ConfigParser()
-    config.read('{}example.ini'.format(config_path))
+    config.read('{}googleMapsApiKey.ini'.format(config_path))  
     key = config['api']['api_number']
 
     #set up address
@@ -36,7 +46,7 @@ def geocode_home(url=''):
     req = urlrequest(url)
     opener = build_opener()
     f = opener.open(req)
-    d = simplejson.load(f)
+    d = json.load(f)
     geocode = [d['results'][0]['geometry']['location']['lat'],
                d['results'][0]['geometry']['location']['lng']]
     return geocode
@@ -48,12 +58,12 @@ def parse_destination_json(d):
 
     addresses = d['destination_addresses']
 
-    print (addresses)
+    print(addresses)
     i = 0
     durations = [0] * len(addresses)
     for row in d['rows'][0]['elements']:
-        if not row['status'] == 'OK':
-            # raise Exception('Error. Google Maps API return status: {}'.format(row['status']))
+        if row['status'] != 'OK':
+            print('Error. Google Maps API return status: {}'.format(row['status']))
             durations[i] = 9999
         else:
             if 'duration_in_traffic' in row:
@@ -62,6 +72,29 @@ def parse_destination_json(d):
                 durations[i] = row['duration']['value'] / 60
         i += 1
     return [addresses, durations]
+
+
+def write_response_file(d, c):
+    myString = ''
+    row = d['rows'][0]['elements']
+
+    for i in range(len(row)):
+        myList = []
+        tmp = ''
+        myList.append(str(c))
+        myList.append(travel_method)
+        myList.append(d['origin_addresses'])
+        myList.append(row[i]['status'])
+
+        myList.append(d['destination_addresses'][i].split())
+        if row[i]['status'] == 'OK':
+            myList.append(row[i]['distance']['value'])
+            myList.append(row[i]['duration']['value'])
+        tmp = ','.join(map(str, myList))
+        myString = myString + tmp + '\n'
+
+    with open(output_path+"statusFile.csv", "a") as text_file:
+        text_file.write(myString)
 
 
 def select_destination(origin='', angle=30, radius=20):
@@ -88,16 +121,16 @@ def select_destination(origin='', angle=30, radius=20):
 def geocode_destinations(origin=[0,0], angles=[0,180], radii = 5 ):
     destinations = []
     for angle, radius in zip(angles,radii):
-        lat2,lng2 = select_destination(origin,angle,radius)
-        print (str(lat2) + "," + str(lng2))
-        destinations.append([lat2,lng2])
+        lat2, lng2 = select_destination(origin, angle, radius)
+        print(str(lat2) + "," + str(lng2))
+        destinations.append([lat2, lng2])
     return destinations
 
 
-def get_travel_times(origin=[0,0], destinations=[0,0], travel_method='DRIVING', config_path='/home/' ):
+def get_travel_times(origin=[0,0], destinations=[0, 0], travel_method='DRIVING', config_path='/home/'):
     #read in api_key
     config = configparser.ConfigParser()
-    config.read('{}example.ini'.format(config_path))
+    config.read('{}googleMapsApiKey.ini'.format(config_path))
     key = config['api']['api_number']
 
     #set up query strings - origin
@@ -117,7 +150,7 @@ def get_travel_times(origin=[0,0], destinations=[0,0], travel_method='DRIVING', 
     req = urlrequest(full_url)
     opener = build_opener()
     f = opener.open(req)
-    d = simplejson.load(f)
+    d = json.load(f)
     return d
 
 
@@ -125,7 +158,6 @@ def get_bearing(origin='', destination=''):
     """
     Calculate the bearing from origin to destination
     """
-
     bearing = atan2(sin((destination[1] - origin[1]) * pi / 180) * cos(destination[0] * pi / 180),
                     cos(origin[0] * pi / 180) * sin(destination[0] * pi / 180) -
                     sin(origin[0] * pi / 180) * cos(destination[0] * pi / 180) * cos((destination[1] - origin[1]) * pi / 180))
@@ -147,41 +179,53 @@ def sort_points(origin='', iso=''):
     sorted_iso = [point[1] for point in sorted_points]
     return sorted_iso
 
+def resolution(r):
+    #based on 12 angles at 30 mins
+    #Each angle has arc length
+    arcLength = 30 * (2 * pi / 12) # 30 mins is my radius, 12 my angles
+    #Length of my circle 
+    circ = 2 * pi * r
+    #number of angles is ratio of two
+    numTheta = ceil(circ / arcLength)
+    return numTheta
+
 
 def main():
-    iso= []
+    initialise()
+    iso = []
     for home in home_string:
-        url = build_url(address_str = home, config_path = config_path )
-        origin =  geocode_home(url)
-        print (str(origin[0]) + "," + str(origin[1]))
+        url = build_url(address_str=home, config_path=config_path)
+        origin = geocode_home(url)
+        print(str(origin[0]) + "," + str(origin[1]))
 
         #make inital destination points
+        no_of_angles = resolution(travel_time_limit) 
         angles =[360 / no_of_angles * i for i in range(no_of_angles)]
-        r = [float(travel_time_limit * 10 / 60)  for _ in range(no_of_angles)]  # at 5mph
+        r = [float(travel_time_limit * 10 / 60) for _ in range(no_of_angles)]  # at 5mph
 
         r_min = [0] * no_of_angles
-        r_max = [float(travel_time_limit * 75 / 60)  for _ in range(no_of_angles)]# at 75mph
+        r_max = [float(travel_time_limit * 75 / 60) for _ in range(no_of_angles)]# at 75mph
 
         counter = 0
         change = True
         while change and counter < 10:
-            print ("Iteration {c}".format(c=counter))
+            print("Iteration {c}".format(c=counter))
             change = False
 
-            destinations = geocode_destinations(origin,angles,r)
-
-            jsonFile = get_travel_times(origin,destinations,travel_method,config_path)
+            destinations = geocode_destinations(origin, angles, r)
+            jsonFile = get_travel_times(origin, destinations, travel_method, config_path)
+            write_response_file(jsonFile, counter)
             travel_times = parse_destination_json(jsonFile)
 
             for i in range(no_of_angles):
-               if travel_times[1][i] < travel_time_limit - tolerance :
-                   r_min[i] = r[i]
-                   r[i] = (r_max[i] + r[i]) / 2
-                   change = True
-               elif travel_times[1][i] > travel_time_limit + tolerance:
-                   r_max[i] = r[i]
-                   r[i] = (r_min[i] + r[i]) / 2
-                   change = True
+                if travel_times[1][i] < travel_time_limit - tolerance:
+                    r_min[i] = r[i]
+                    r[i] = (r_max[i] + r[i]) / 2
+                    change = True
+                elif travel_times[1][i] > travel_time_limit + tolerance:
+                    r_max[i] = r[i]
+                    r[i] = (r_min[i] + r[i]) / 2
+                    change = True
             counter += 1
 
         destinations = geocode_destinations(origin, angles, r)
@@ -192,8 +236,8 @@ def main():
     mystring = ''
     for el in iso:
         for i in el:
-            mystring = mystring + '\n' + str(i) +','
-        mystring = mystring[:-1] +'\n], \n['
+            mystring = mystring + '\n' + str(i) + ','
+        mystring = mystring[:-1] + '\n], \n['
     mystring = 'var bounds = [[' + mystring[:-6] + '\n]];'
 
     with open(output_path+"/bounds.js", "w") as text_file:
@@ -201,4 +245,4 @@ def main():
 #end
 
 
-main
+main()
